a816b899165084c63ea28a520c2b06e9b6ccf26a
Maciek Korzeniewski <mack0r3@gmail.com>
2016-02-09 11:04:15 +0100

    Resources file has been added ( like colors, strings ). Moreover I updated MainActivity.java and activity_main layout in order to workout with previous commit

--- app/src/main/java/com/example/mackor/ezstudies/MainActivity.java
+++ app/src/main/java/com/example/mackor/ezstudies/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.mackor.ezstudies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button registerButton = (Button)findViewById(R.id.buttonRegister);
+
+        registerButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
